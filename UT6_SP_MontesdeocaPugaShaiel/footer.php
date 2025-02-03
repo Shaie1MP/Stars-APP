43b65e40649abde62f91ab044c3e6777c5933748
Shaie1MP <shaielda2003@gmail.com>
2025-02-03 18:59:47 +0000

    Delete UT6_SP_MontesdeocaPugaShaiel directory

--- UT6_SP_MontesdeocaPugaShaiel/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> STARS APP. Todos los derechos reservados.</p>
-        </div>
-    </footer>
-</body>
-</html>
